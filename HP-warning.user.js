e4f6fe0fd6742b74856c0dea5ff00c422281e0ce
Martin Rocek <martin@oculum.ink>
2016-07-10 15:17:37 +0200

    1.0.3

--- HP-warning.user.js
+++ HP-warning.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Isleward - HP Warning
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Warn you when you HP get low
 // @author       Silence.sys
 // @match        *.play.isleward.com/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @resource     HPwarn https://cdn.rawgit.com/Isleward-Addon-Dev-Group/hp-warning/1.0.2/hp-warn.css
+// @resource     HPwarn https://cdn.rawgit.com/Isleward-Addon-Dev-Group/hp-warning/1.0.3/hp-warn.css
 // ==/UserScript==
 
 (function() {
@@ -15,6 +15,6 @@
     GM_addStyle(css);
     var scriptElement = document.createElement( "script" );
     scriptElement.type = "text/javascript";
-    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/hp-warning/1.0.2/hp-warn.js";
+    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/hp-warning/1.0.3/hp-warn.js";
     document.body.appendChild( scriptElement );
 })();
